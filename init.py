b3fb59430a14adb92cb28ee2bc3ec5e53c0c6358
lixiyuan9420 <20191112413@stu.sspu.edu.cn>
2022-08-10 13:43:03 +0800

    debug

--- init.py
+++ init.py
@@ -1,3 +1,4 @@
+from apscheduler.schedulers.background import BackgroundScheduler
 from flask_apscheduler import APScheduler
 
-scheduler = APScheduler()
+scheduler = APScheduler(scheduler=BackgroundScheduler(timezone='Asia/Shanghai'))
