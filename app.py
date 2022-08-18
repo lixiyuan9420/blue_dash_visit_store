c2c049ff6f189e02f4b82ce98a86fbec5b33df9d
lixiyuan9420 <20191112413@stu.sspu.edu.cn>
2022-08-18 14:50:06 +0800

    debug

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import biz_logic
 
 # 创建一个Flask app对象
 from init import scheduler
-from util.request_hander.store_record import send_message_book, send_message_total
+from util.request_hander.store_record import send_message_book, send_message_total, confirm_address
 
 app = Flask(__name__)
 
@@ -22,4 +22,5 @@ if __name__ == '__main__':
     scheduler.add_job(func=send_message_total,id='1', trigger='interval',days = 1,start_date = '2022-08-18 09:00:00')
     scheduler.init_app(app=app)
     scheduler.start()
+    confirm_address("腾飞")
     app.run(host='0.0.0.0',port=3392)
