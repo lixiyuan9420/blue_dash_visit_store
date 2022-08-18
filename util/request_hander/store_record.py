8264a29022e27e6daf3b5cb98f29d0944b176ddd
lixiyuan9420 <20191112413@stu.sspu.edu.cn>
2022-08-18 16:58:49 +0800

    debug

--- util/request_hander/store_record.py
+++ util/request_hander/store_record.py
@@ -358,14 +358,14 @@ def confirm_address(address):
             # 遍历该列表
         for i in data:
             # 以append的方式不断写入到csv文件中
-            with open("mdjcxx.csv",'r+', encoding='gb18030') as user:
+            with open("mdjcxx.csv",'r+', encoding='utf-8') as user:
                 # 写入文件时增加换行符，保证每个元素位于一行
                 user.write(str(i) + '\n')
         for i in find:
-            with open("new.csv", 'r+', encoding='gb18030') as name:
+            with open("new.csv", 'r+', encoding='utf-8') as name:
                 # 写入文件时增加换行符，保证每个元素位于一行
                 name.write(str(i) + '\n')
-        data = pd.read_csv("mdjcxx.csv",error_bad_lines=False)
+        data = pd.read_csv("mdjcxx.csv")
         find = pd.read_csv("new.csv")
         data_split_word = data.user.apply(jieba.lcut)
         dictionary = corpora.Dictionary(data_split_word.values)
